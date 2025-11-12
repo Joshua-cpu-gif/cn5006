0bc5f4c6868e7c41179972807e55221225001c60
Joshua-cpu-gif <lamurentv84@gmail.com>
2025-11-12 12:14:04 +0000

    week 7 work done

---
+++ week7/hookexample/src/emojicounter.js
@@ -0,0 +1,7 @@
+import React, {use useState , useEffect}from "react";
+import Love from "./Love.png";
+import Sad from "./Sad.png";
+import Like from "./Like.png";
+import happy from "./happy.png";
+
+}
